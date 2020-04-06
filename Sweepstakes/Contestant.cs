061e276e8e743ac1ece08a6b8d895c47df14bf4c
Christopher Reding <creding21@gmail.com>
2020-04-06 11:22:09 -0500

    set up initial classes

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        //member variables
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber;
+    }
+}
